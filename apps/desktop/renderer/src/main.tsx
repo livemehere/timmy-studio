07da0fc905f4f44c5a0a5a37a357196c44426d9d
livemehere <livemehere@users.noreply.github.com>
2025-10-27 21:49:19 +0900

    refactor: clean up code structure and remove redundant sections

--- apps/desktop/renderer/src/main.tsx
+++ apps/desktop/renderer/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
-
+import '@renderer/index.css';
 import App from '@renderer/App';
 
 createRoot(document.getElementById('root')!).render(<App />);
--- apps/desktop/renderer/src/pages/home-page.tsx
+++ apps/desktop/renderer/src/pages/home-page.tsx
@@ -5,10 +5,10 @@ import { getPublicPath } from '@timmy-studio/electron-utils/utils/renderer';
 export function HomePage() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>IPC Examples</h1>
+      <h1 className="text-4xl font-bold text-red-400">IPC Examples</h1>
       <img src={getPublicPath('/vite.svg')} alt="" />
       <section style={{ marginBottom: '20px' }}>
-        <h2>Basic Invoke</h2>
+        <h2 className="text-2xl font-bold">Basic Invoke</h2>
         <Suspense fallback={<div>Loading App Info...</div>}>
           <AppInfo />
         </Suspense>
