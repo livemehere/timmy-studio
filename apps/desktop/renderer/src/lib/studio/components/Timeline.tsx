6d1500acebbceae266ab7a4fc22af09afb827108
livemehere <livemehere@users.noreply.github.com>
2025-11-26 04:12:25 +0900

    feat(timeline): enhance timeline editing

--- apps/desktop/renderer/src/lib/studio/components/Timeline.tsx
+++ apps/desktop/renderer/src/lib/studio/components/Timeline.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useDocSettings, useEngineTimer } from '../hooks';
+
 export function Timeline() {
-  return <div>Timeline</div>;
+  const settings = useDocSettings();
+  const timer = useEngineTimer();
+  const [currentMs, setCurrentMs] = useState(0);
+  useEffect(() => {
+    if (!timer) return;
+
+    return timer.subscribe(({ currentMs }) => {
+      setCurrentMs(currentMs);
+    });
+  }, [timer]);
+
+  return (
+    <div>
+      <input
+        className="w-full"
+        type="range"
+        value={currentMs}
+        onChange={(e) => {
+          const v = Number(e.target.value);
+          timer?.seek(v);
+        }}
+        min={0}
+        step={100}
+        max={settings.duration}
+      />
+    </div>
+  );
 }
