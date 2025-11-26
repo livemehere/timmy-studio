4db4a18fd9253872888e6de06d699dc79c2a7ea6
livemehere <livemehere@users.noreply.github.com>
2025-11-27 03:57:39 +0900

    refactor(timeline): reorganize timeline editing

--- apps/desktop/renderer/src/lib/studio/components/TimelineRulerCanvas.tsx
+++ apps/desktop/renderer/src/lib/studio/components/TimelineRulerCanvas.tsx
@@ -9,42 +9,35 @@ export function TimelineRulerCanvas({
   scrollXMotionValue: MotionValue<number>;
   leftPadding: number;
 }) {
-  const settings = useDocSettings();
-  const timer = useEngineTimer();
-  const [currentMs, setCurrentMs] = useState(0);
-  useEffect(() => {
-    if (!timer) return;
-
-    return timer.subscribe(({ currentMs }) => {
-      setCurrentMs(currentMs);
-    });
-  }, [timer]);
+  const { duration } = useDocSettings();
 
   return (
-    <div
-      className={'w-full h-[12px] select-none'}
-      style={{
-        paddingLeft: leftPadding,
-      }}
-    >
-      <motion.div
+    <div className={'w-full h-[20px] select-none '}>
+      <div
+        className={'h-full border-t-orange-400/30 border-t'}
         style={{
-          left: scrollXMotionValue,
+          marginLeft: leftPadding,
         }}
-        className={'w-1 bg-red-500 h-full relative'}
-      ></motion.div>
-      {/*<input*/}
-      {/*  className="w-full"*/}
-      {/*  type="range"*/}
-      {/*  value={currentMs}*/}
-      {/*  onChange={(e) => {*/}
-      {/*    const v = Number(e.target.value);*/}
-      {/*    timer?.seek(v);*/}
-      {/*  }}*/}
-      {/*  min={0}*/}
-      {/*  step={100}*/}
-      {/*  max={settings.duration}*/}
-      {/*/>*/}
+      >
+        <motion.div
+          style={{
+            left: scrollXMotionValue,
+          }}
+          className={'w-1 bg-red-500 h-full relative'}
+        ></motion.div>
+        {/*<input*/}
+        {/*  className="w-full"*/}
+        {/*  type="range"*/}
+        {/*  value={currentMs}*/}
+        {/*  onChange={(e) => {*/}
+        {/*    const v = Number(e.target.value);*/}
+        {/*    timer?.seek(v);*/}
+        {/*  }}*/}
+        {/*  min={0}*/}
+        {/*  step={100}*/}
+        {/*  max={settings.duration}*/}
+        {/*/>*/}
+      </div>
     </div>
   );
 }
