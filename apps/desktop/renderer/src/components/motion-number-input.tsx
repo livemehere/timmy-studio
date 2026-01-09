8abd5125f8a0eb5e8bfe0ff61b917b3f08896ca1
livemehere <livemehere@users.noreply.github.com>
2026-01-10 04:21:55 +0900

    feat(renderer): enhance preview rendering

---
+++ apps/desktop/renderer/src/components/motion-number-input.tsx
@@ -0,0 +1,58 @@
+import { motion, type MotionValue, useMotionValueEvent } from 'motion/react';
+import { useRef } from 'react';
+import { cn } from '@/lib/utils';
+
+function MotionNumberInput({
+  icon,
+  value,
+  onChange,
+  map = (v) => v,
+}: {
+  icon?: React.ReactNode;
+  value: MotionValue<number>;
+  onChange: (v: number) => void;
+  map?: (v: number) => number;
+}) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useMotionValueEvent(value, 'change', (v) => {
+    onChange(v);
+    ref.current!.value = String(map(v));
+  });
+
+  return (
+    <div
+      className={cn(
+        'flex items-center gap-2 focus-within:outline-1 px-2 py-1 border border-neutral-600 rounded'
+      )}
+    >
+      <motion.div
+        drag={'x'}
+        className={'shrink-0 px-1 cursor-ew-resize select-none'}
+        dragConstraints={{ left: 0, right: 0 }}
+        dragElastic={false}
+        onDrag={(_, info) => {
+          const newValue = map(value.get() + info.delta.x);
+          value.set(newValue);
+          ref.current!.value = String(newValue);
+        }}
+      >
+        {icon}
+      </motion.div>
+      <input
+        ref={ref}
+        type={'number'}
+        defaultValue={value.get()}
+        onChange={(e) => {
+          const v = map(Number(e.target.value));
+          value.set(v);
+        }}
+        className={
+          'outline-none flex-1 [&::-webkit-inner-spin-button]:appearance-none w-full'
+        }
+      />
+    </div>
+  );
+}
+
+export { MotionNumberInput };
--- apps/desktop/renderer/src/pages/pixi-playground.tsx
+++ apps/desktop/renderer/src/pages/pixi-playground.tsx
@@ -1,11 +1,51 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ButtonGroup } from '@/components/ui/button-group';
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import * as PIXI from 'pixi.js';
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from '@/components/ui/field';
+import {
+  motion,
+  type MotionValue,
+  useMotionValue,
+  useMotionValueEvent,
+} from 'motion/react';
+import { cn } from '@/lib/utils';
+import { Link2 } from 'lucide-react';
+import { MotionNumberInput } from '@/components/motion-number-input';
 
 export default function PixiPlaygroundPage() {
   const parentRef = useRef<HTMLDivElement>(null);
   const appRef = useRef<PIXI.Application | null>(null);
+
+  const spriteRef = useRef<PIXI.Sprite | null>(null);
+
+  // sprite values
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const width = useMotionValue(0);
+  const height = useMotionValue(0);
+  const [sizeChained, setSizeChained] = useState(false);
+  // ---
+
+  const getApp = () => {
+    return appRef.current!;
+  };
 
   const create = async (width: number, height: number) => {
     const app = new PIXI.Application();
@@ -33,10 +73,29 @@ export default function PixiPlaygroundPage() {
     app.canvas.style.display = 'block';
 
     parentRef.current!.appendChild(app.canvas);
+
+    await initRender();
   };
 
   const destroy = async () => {
     appRef.current!.destroy(true);
+    appRef.current = null;
+  };
+
+  const initRender = async () => {
+    const image = new Image();
+    image.src = 'file:///path/to/sample-media.png';
+    await image.decode();
+    const texture = PIXI.Texture.from(image);
+    const sprite = new PIXI.Sprite(texture);
+    spriteRef.current = sprite;
+
+    getApp().stage.addChild(sprite);
+
+    x.set(sprite.x);
+    y.set(sprite.y);
+    width.set(sprite.width);
+    height.set(sprite.height);
   };
 
   useEffect(() => {
@@ -47,11 +106,7 @@ export default function PixiPlaygroundPage() {
   }, []);
 
   return (
-    <div
-      className={
-        'relative h-full flex flex-col items-center justify-center gap-4'
-      }
-    >
+    <div className={'relative h-full flex items-center justify-center gap-4'}>
       <div
         className="absolute inset-0 z-0"
         style={{
@@ -60,22 +115,106 @@ export default function PixiPlaygroundPage() {
         }}
       />
 
-      <h1 className="relative z-10">PIXI</h1>
-      <ButtonGroup className="relative z-10">
-        <Button variant={'outline'} onClick={() => create(1920, 1080)}>
-          Create
-        </Button>
-        <Button variant={'outline'} onClick={destroy}>
-          Destroy
-        </Button>
-      </ButtonGroup>
+      <div className="relative z-10 w-full grid grid-cols-[350px_1fr_350px] gap-4 px-4">
+        <div></div>
 
-      <div
-        ref={parentRef}
-        className={
-          'relative border border-neutral-50 w-1/2 h-1/2 bg-neutral-900 flex items-center justify-center'
-        }
-      ></div>
+        <div className={'flex flex-col items-center gap-4'}>
+          <ButtonGroup className="relative z-10">
+            <Button variant={'outline'} onClick={() => create(1920, 1080)}>
+              Create
+            </Button>
+            <Button variant={'outline'} onClick={destroy}>
+              Destroy
+            </Button>
+          </ButtonGroup>
+          {/* 가운데 - Renderer */}
+          <div
+            ref={parentRef}
+            className={
+              'relative border border-neutral-50 bg-neutral-900 flex items-center justify-center'
+            }
+          ></div>
+        </div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Properties</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FieldSet>
+              <FieldLegend>Position</FieldLegend>
+              <div className={'flex gap-2'}>
+                <MotionNumberInput
+                  value={x}
+                  map={(v) => Number(v.toFixed(2))}
+                  onChange={(v) => {
+                    spriteRef.current!.x = v;
+                  }}
+                  icon={<div className={'text-sm'}>X</div>}
+                />
+                <MotionNumberInput
+                  value={y}
+                  map={(v) => Number(v.toFixed(2))}
+                  onChange={(v) => {
+                    spriteRef.current!.y = v;
+                  }}
+                  icon={<div className={'text-sm'}>Y</div>}
+                />
+              </div>
+            </FieldSet>
+            <FieldSet className={'mt-4'}>
+              <FieldLegend>Size</FieldLegend>
+              <div className={'flex gap-2'}>
+                <Button
+                  size={'icon-sm'}
+                  variant={'outline'}
+                  className={cn({
+                    '[&_svg]:stroke-blue-500': sizeChained,
+                  })}
+                  onClick={() => {
+                    setSizeChained(!sizeChained);
+                  }}
+                >
+                  <Link2 size={16} />
+                </Button>
+                <MotionNumberInput
+                  value={width}
+                  map={(v) => Math.max(0, Number(v.toFixed(2)))}
+                  onChange={(v) => {
+                    spriteRef.current!.width = v;
+
+                    if (sizeChained) {
+                      const aspectRatio =
+                        spriteRef.current!.texture.width /
+                        spriteRef.current!.texture.height;
+                      const newHeight = v / aspectRatio;
+                      spriteRef.current!.height = newHeight;
+                      height.set(newHeight);
+                    }
+                  }}
+                  icon={<div className={'text-sm'}>W</div>}
+                />
+                <MotionNumberInput
+                  value={height}
+                  map={(v) => Math.max(0, Number(v.toFixed(2)))}
+                  onChange={(v) => {
+                    spriteRef.current!.height = v;
+                    if (sizeChained) {
+                      const aspectRatio =
+                        spriteRef.current!.texture.width /
+                        spriteRef.current!.texture.height;
+                      const newWidth = v * aspectRatio;
+                      spriteRef.current!.width = newWidth;
+                      width.set(newWidth);
+                    }
+                  }}
+                  icon={<div className={'text-sm'}>H</div>}
+                />
+              </div>
+            </FieldSet>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
