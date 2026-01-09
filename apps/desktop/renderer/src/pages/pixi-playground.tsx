df9fde6b8fd24a8d21d5c69440f774de41ac6b42
livemehere <livemehere@users.noreply.github.com>
2026-01-10 01:14:34 +0900

    feat(renderer): enhance preview rendering

--- apps/desktop/renderer/src/pages/pixi-playground.tsx
+++ apps/desktop/renderer/src/pages/pixi-playground.tsx
@@ -1,7 +1,31 @@
+import { ButtonGroup } from '@/components/ui/button-group';
+import { Button } from '@/components/ui/button';
+
 export default function PixiPlaygroundPage() {
   return (
-    <div className={'bg-neutral-700 h-full'}>
-      <h1>PIXI</h1>
+    <div
+      className={
+        'relative h-full flex flex-col items-center justify-center gap-4'
+      }
+    >
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background:
+            'radial-gradient(125% 125% at 50% 100%, #000000 40%, #010133 100%)',
+        }}
+      />
+
+      <h1 className="relative z-10">PIXI</h1>
+      <ButtonGroup className="relative z-10">
+        <Button variant={'outline'}>A</Button>
+        <Button variant={'outline'}>B</Button>
+        <Button variant={'outline'}>C</Button>
+      </ButtonGroup>
+
+      <div
+        className={'relative z-10 border border-neutral-50 w-1/2 h-1/2'}
+      ></div>
     </div>
   );
 }
